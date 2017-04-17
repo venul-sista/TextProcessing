a94f297babade92150aaceccb17cd599ce5878bb
unknown <gopal.sista@gmail.com>
2017-04-17 23:14:56 +0530

    First commit

---
+++ Hadoop/src/com/venu/DoctorToPatient.java
@@ -0,0 +1,75 @@
+package com.venu;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import com.venu.WordCount.Map;
+import com.venu.WordCount.Reduce;
+
+public class DoctorToPatient {
+
+	/*
+	 * 1st 2 parameters are input and last 2 parameters are output. map
+	 * functions --> Context object is used to write the output
+	 */
+	public static class Map extends Mapper<LongWritable, Text, IntWritable, IntWritable> {
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			while (tokenizer.hasMoreTokens()) {
+//				value.set(tokenizer.nextToken());
+//				context.write(value, new IntWritable(1));
+				value.set(tokenizer.nextToken());
+				context.write(new IntWritable(value.getLength()), new IntWritable(1));
+				
+			}
+		}
+	}
+
+	public static class Reduce extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+		public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
+			for(IntWritable x : values) {
+				sum += x.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = new Job(conf,"mywc");
+		
+		job.setJarByClass(DoctorToPatient.class);
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		System.exit(job.waitForCompletion(true)?0:1);
+		
+	}
+}
